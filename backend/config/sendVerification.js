45a02c4b2ff3ddd49201fdec9a6d1e1c884f5bf4
saignanadeep11 <saignanadeep11@gmail.com>
2024-10-13 21:56:18 +0530

    email

--- backend/config/sendVerification.js
+++ backend/config/sendVerification.js
@@ -22,8 +22,16 @@ const sendVerificationEmail = async (cUser) => {
     from: "career.folio.services@gmail.com",
     to: cUser.email,
     subject: "Email Verification",
-    text: `Please verify your email by clicking this link: ${verificationUrl}`,
-    html: `<h1>Hello ${cUser.name}</h1><p>Please verify your email by clicking <a href="${verificationUrl}">this link</a>.</p><p>For any queries please contact us</p>`,
+    text: `Hello ${cUser.name},
+    Thank you for signing up for career folio.
+    Please verify your email by clicking this link: ${verificationUrl} . This link will expire in 1 hour. If you did not sign up for career folio please ignore this mail. 
+    For any queries please contact us. `,
+    html: `<h1>Hello ${cUser.name}</h1>
+    <p>Thank you for signing up for career folio.</p>
+
+    <p>Please verify your email by clicking <a href="${verificationUrl}">this link</a>.
+    <p>This link will expire in 1 hour. If you did not sign up for career folio please ignore this mail. </p>
+    </p><p>For any queries please contact us.</p>`,
   };
 
   await transporter.sendMail(mailOptions);
